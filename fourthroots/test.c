609bd233d48f38205b25f8afc25e00a04fd3bd61
Dmitry Bogdanov <dbogdanov@marvell.com>
2020-10-09 00:58:03 +0300

    add test on small values

--- fourthroots/test.c
+++ fourthroots/test.c
@@ -5,62 +5,69 @@
 
 #include "fourth_roots.h"
 
-#define EPSILON __FLT_EPSILON__
-
-#define CHECK_INT(COND, EXPECTED) \
+#define CHECK_INT(COND, EXPECTED) ({\
+    int _ret = 0; \
     do {\
         typeof(COND) _cond = COND; \
         if (_cond != (EXPECTED)) \
             printf("%s = %d instead of %d\n", #COND, _cond, (EXPECTED)) ; \
-    } while (0)
+    } while (0); \
+    _ret; })
 
-#define CHECK_FLOAT(COND, EXPECTED) \
+#define CHECK_FLOAT(COND, EXPECTED, EPSILON) ({ \
+    int _ret = 0; \
     do {\
         typeof(COND) _cond = COND; \
-        if (fabs(_cond - (EXPECTED)) > EPSILON) \
+        printf("checking %.38f against %.38f\n", _cond, EXPECTED);\
+        if (fabs(_cond - (EXPECTED)) > EPSILON) { \
             printf("%s = %.38f instead of %.38f\n", #COND, _cond, (EXPECTED)); \
-    } while (0)
+            _ret = -1; \
+        } \
+    } while (0); \
+    (ret); })
 
 struct test_case_t {
     unsigned int len;
+    float epsilon;
     float vec[];
 };
 
-static struct test_case_t test_vec_1 = {1, 16.0};
-static struct test_case_t test_vec_2 = {2, {16.0, 81.0}};
-static struct test_case_t test_vec_max = {1, __FLT_MAX__};
-static struct test_case_t test_vec_0 = {0, 0};
-static struct test_case_t test_vec_09 = {4, 0.6561, 0.00006561, 0.000000006561, 0.0000000000006561};
-static struct test_case_t test_vec_min = {1, __FLT_MIN__};
-struct test_case_t test_vec_neg = {1, -1.0};
+static struct test_case_t test_vec_1 = {1, __FLT_EPSILON__, 16.0};
+static struct test_case_t test_vec_2 = {2, __FLT_EPSILON__, {16.0, 81.0}};
+static struct test_case_t test_vec_max = {1, __FLT_EPSILON__, __FLT_MAX__};
+static struct test_case_t test_vec_0 = {0, __FLT_EPSILON__, 0};
+static struct test_case_t test_vec_09 = {4, __FLT_EPSILON__/ 1e7, {0.6561, 0.00006561, 0.000000006561, 0.0000000000006561}};
+static struct test_case_t test_vec_min = {5, __FLT_EPSILON__ / 1e10, {__FLT_EPSILON__, __FLT_EPSILON__*__FLT_EPSILON__, __FLT_EPSILON__*__FLT_EPSILON__*__FLT_EPSILON__*__FLT_EPSILON__, __FLT_EPSILON__*__FLT_EPSILON__*__FLT_EPSILON__*__FLT_EPSILON__*__FLT_EPSILON__, __FLT_MIN__}};
+struct test_case_t test_vec_neg = {1, __FLT_EPSILON__, -1.0};
 
 static int test_vec(struct test_case_t *tc)
 {
     float *res = malloc(tc->len * sizeof(float));
+    int ret = 0;
 
     if (!res)
         return -1;
 
-    CHECK_INT(compute_fourth_roots(tc->vec, res, tc->len, EPSILON), 0);
+    CHECK_INT(compute_fourth_roots(tc->vec, res, tc->len, tc->epsilon), 0);
     for(int i = 0; i < tc->len; i++)
-        CHECK_FLOAT(res[i], (float)sqrt(sqrt(tc->vec[i])));
+        ret |= CHECK_FLOAT(res[i], (float)sqrt(sqrt(tc->vec[i])), tc->epsilon);
 
-    return 0;
+    return ret;
 }
 
 static int test_neg(struct test_case_t *tc)
 {
     float *res = malloc(tc->len * sizeof(float));
+    int ret = 0;
 
     if (!res)
         return -1;
 
-    CHECK_INT(compute_fourth_roots(tc->vec, res, tc->len, EPSILON), 0);
+    CHECK_INT(compute_fourth_roots(tc->vec, res, tc->len, tc->epsilon), 0);
     for(int i = 0; i < tc->len; i++)
-        CHECK_INT(_isnanf(res[i]), 1);
+        ret |= CHECK_INT(_isnanf(res[i]), 1);
 
-
-    return 0;
+    return ret;
 }
 
 int tests()
@@ -72,6 +79,7 @@ int tests()
     res |= test_vec(&test_vec_max);
     res |= test_vec(&test_vec_0);
     res |= test_vec(&test_vec_09);
+    res |= test_vec(&test_vec_min);
     res |= test_neg(&test_vec_neg);
 
     if (!res)

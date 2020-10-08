a69ff69115a5305802c544fd357caf3db80d9979
Dmitry Bogdanov <dbogdanov@marvell.com>
2020-10-09 01:04:27 +0300

    add simple performance test

--- fourthroots/main.c
+++ fourthroots/main.c
@@ -11,8 +11,35 @@ a test function that checks the outcome of the processing function
 ‘main’ function that generates input data for the processing functions and checks the processing outcome
  
 */
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <errno.h>
+#include "fourth_roots.h"
 #include "test.h"
+
 int main(int argc, char** argv)
 {
-    return tests();
+    if (argc == 2 && argv[1][0]=='t')
+        return tests();
+
+    float *vec = malloc(1000000*sizeof(float));
+    float *res = malloc(1000000*sizeof(float));
+    if (!vec || !res)
+        return -ENOMEM;
+
+    for (int i = 0; i < 1000000; i++) {
+        vec[i] =  (float)rand()/(float)(__FLT_MAX__);;
+    }
+
+    {
+        clock_t start = clock(), diff;
+        compute_fourth_roots(vec, res, 1000000, 0.000001);
+        diff = clock() - start;
+
+        int msec = diff * 1000 / CLOCKS_PER_SEC;
+        printf("Calcultaing 1 000 000 fourth roots taken %d seconds %d milliseconds", msec/1000, msec%1000);
+    }
+
+    return 0;
 }
